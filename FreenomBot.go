01414b0caf01a7576946ac59135d01c979597991
frank30941 <frank30941@gmail.com>
2020-09-06 20:58:07 +0800

    Fix
    Fix multi-user feature
    Change cron tool

--- FreenomBot.go
+++ FreenomBot.go
@@ -7,14 +7,16 @@ import (
 	freenom "github.com/codesensegroup/FreenomBot/internal/freenom"
 	httpservice "github.com/codesensegroup/FreenomBot/server/httpservice"
 
-	"github.com/robfig/cron"
+	//"github.com/robfig/cron"
+	"github.com/jasonlvhit/gocron"
 )
 
-func runFreenom(run func()) {
-	c := cron.New()
-	c.AddFunc("* * 23 */1 * *", run)
-	c.Start()
-	select {}
+var cronJobs = make(map[int]*gocron.Scheduler)
+
+func runFreenom(id int, run func(id int)) {
+	cronJobs[id] = gocron.NewScheduler()
+	cronJobs[id].Every(1).Day().At("06:30").Do(run, id)
+	<-cronJobs[id].Start()
 }
 
 func main() {
@@ -22,21 +24,20 @@ func main() {
 	config, _ := checkprofile.ReadConf("./config.toml")
 	f := freenom.GetInstance()
 
-	go func() {
-		for i, a := range config.Accounts {
-			f.InputAccount(i, a.Username, a.Password)
-			f.Login(i).RenewDomains(i)
-			for _, d := range f.Users[i].Domains {
+	for i, a := range config.Accounts {
+		f.InputAccount(i, a.Username, a.Password)
+		f.Login(i).RenewDomains(i)
+		for _, d := range f.Users[i].Domains {
+			log.Println("log: ", d)
+		}
+		//Use goroutine
+		go runFreenom(i, func(id int) {
+			f.Login(id).RenewDomains(id)
+			for _, d := range f.Users[id].Domains {
 				log.Println("log: ", d)
 			}
-			runFreenom(func() {
-				f.Login(i).RenewDomains(i)
-				for _, d := range f.Users[i].Domains {
-					log.Println("log: ", d)
-				}
-			})
-		}
-	}()
+		})
+	}
 
 	httpservice.Run(f)
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.14
 
 require (
 	github.com/BurntSushi/toml v0.3.0
+	github.com/jasonlvhit/gocron v0.0.1
 	github.com/nicksnyder/go-i18n/v2 v2.0.3 // indirect
-	github.com/robfig/cron v1.2.0
-	github.com/robfig/cron/v3 v3.0.0 // indirect
 	github.com/tidwall/gjson v1.6.0
 	github.com/tidwall/pretty v1.0.1 // indirect
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381
